35308faff65a44adb9db93e67e7e9f2e84391ae4
Christopher Eby <kreed@kreed.org>
2016-01-10 11:38:19 -0600

    deal with overlapping partition issues some WCS licenses have
    Some WCS licenses have strange partitions partitions specified. For
    example, KNLB236 has two partitions: one covering for whole market
    with freqency 2350-2355, and a smaller partition partition covering
    only New Orleans with frequencies 2305-2310 and 2350-2355. Current
    processing results in overlapping geometries in New Orleans that both
    specify frequency 2350-2355.
    
    The ULS map tool errors out on licenses like this, so this is probably a
    data issue. But until these licenses are fixed, this commit adds some
    extra geometry processing to deal with the overlaps.

--- partcollection.py
+++ partcollection.py
@@ -9,6 +9,28 @@ class PartitionCollection:
 			geom = unary_union([ p[0] for p in parts ])
 			pops = [ p[1] for p in parts ]
 			pop = None if None in pops else sum(pops)
+
+			intersects = 0
+			for f in list(self.parts.keys()): # copy the key list because we may change the dict
+				new_contains_existing = freq.contains(f)
+				existing_contains_new = f.contains(freq)
+				if not new_contains_existing and not existing_contains_new:
+					continue
+
+				intersection = geom.intersection(self.parts[f][0])
+
+				if intersection.area / geom.area > .01:
+					intersects += 1
+
+					if existing_contains_new:
+						geom.difference(intersection)
+						pop = None
+					else:
+						self.parts[f] = (self.parts[f][0].difference(intersection), None)
+
+			if intersects > 0:
+				print('add intersects:', intersects)
+
 			self.parts[freq] = (geom, pop)
 
 	def subtract_parts(self, table):
@@ -17,15 +39,17 @@ class PartitionCollection:
 
 			intersects = 0
 			remaining_geom = geom
-			for f in list(self.parts.keys()):
-				if not f.contains(freq):
+			for f in list(self.parts.keys()): # copy the key list because we may change the dict
+				add_contains = f.contains(freq)
+				sub_contains = freq.contains(f)
+				if not add_contains and not sub_contains:
 					continue
 
 				intersection = remaining_geom.intersection(self.parts[f][0])
 				if intersection.area / geom.area > .01:
 					intersects += 1
 
-					if f != freq:
+					if add_contains and not sub_contains:
 						add_freq = f.difference(freq)
 						if add_freq in self.parts:
 							add_geom, add_pop = self.parts[add_freq]
@@ -41,7 +65,7 @@ class PartitionCollection:
 						break
 
 			if intersects > 1:
-				print('intersects:', intersects)
+				print('subtract intersects:', intersects)
 
 			if remaining_geom.area / geom.area >= .01:
 				print("large remaining exclude geometry (%.3f%% of original area)" % (100 * remaining_geom.area / geom.area))
